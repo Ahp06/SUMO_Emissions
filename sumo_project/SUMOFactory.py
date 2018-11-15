b33d3b750ace0aae79953f269e4a12bba032a083
Thibaud <thibaud.gasser@gmx.com>
2018-11-15 13:51:46 +0100

    Delete SUMOFactory class

--- sumo_project/SUMOFactory.py
+++ sumo_project/SUMOFactory.py
@@ -8,21 +8,21 @@ import traci
 from shapely.geometry.linestring import LineString
 
 
-class SUMOFactory(object):
-    
-    def stop_vehicle(self, veh_id):
-        traci.vehicle.remove(veh_id, traci.constants.REMOVE_PARKING)
+def stop_vehicle(veh_id):
+    traci.vehicle.remove(veh_id, traci.constants.REMOVE_PARKING)
 
-    def lanes_in_area(self, area):
-        for lane_id in traci.lane.getIDList():
-            polygon_lane = LineString(traci.lane.getShape(lane_id))
-            if area.rectangle.intersects(polygon_lane):
-                yield lane_id
-           
-    def lock_area(self, area):
-        for lane_id in self.lanes_in_area(area):
-            print(f'Setting max speed of {lane_id} to 9.')
-            traci.lane.setMaxSpeed(lane_id, 9)
 
-        for veh_id in traci.vehicle.getIDList():
-            traci.vehicle.rerouteTraveltime(veh_id, True)
+def lanes_in_area(area):
+    for lane_id in traci.lane.getIDList():
+        polygon_lane = LineString(traci.lane.getShape(lane_id))
+        if area.rectangle.intersects(polygon_lane):
+            yield lane_id
+
+
+def lock_area(area):
+    for lane_id in lanes_in_area(area):
+        print(f'Setting max speed of {lane_id} to 30.')
+        traci.lane.setMaxSpeed(lane_id, 30)
+
+    for veh_id in traci.vehicle.getIDList():
+        traci.vehicle.rerouteTraveltime(veh_id, True)
